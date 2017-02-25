bb990c2ab8427b4099e1906f8fb7d6c8a37babff
git-prodigy <aaditya.prodigy@gmail.com>
2017-02-25 22:15:52 +0530

    Create start-all.bat

---
+++ start-all.bat
@@ -0,0 +1,12 @@
+@ECHO OFF 
+start vmrun start "C:\work\VMs\Virtual Machines\Local_HUE\Local_HUE.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_Hadoop_64-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode1_64-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode2_64-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode3-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode4_64-bit\Local_Hadoop_64-bit.vmx"
