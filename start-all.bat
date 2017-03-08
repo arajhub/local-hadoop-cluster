a495a4451691ec38e4164a3a7eeae311eeff3d7f
NAM\araj015 <aaditya.raj@pwc.com>
2017-03-08 16:19:59 +0530

    chnaged order

--- start-all.bat
+++ start-all.bat
@@ -1,8 +1,4 @@
 @ECHO OFF 
-start vmrun start "C:\work\VMs\Virtual Machines\Local_HUE\Local_HUE.vmx"
-TIMEOUT /T 10
-start vmrun start "C:\work\VMs\Virtual Machines\Local_Hadoop_64-bit\Local_Hadoop_64-bit.vmx"
-TIMEOUT /T 10
 start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode1_64-bit\Local_Hadoop_64-bit.vmx"
 TIMEOUT /T 10
 start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode2_64-bit\Local_Hadoop_64-bit.vmx"
@@ -10,3 +6,7 @@ TIMEOUT /T 10
 start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode3-bit\Local_Hadoop_64-bit.vmx"
 TIMEOUT /T 10
 start vmrun start "C:\work\VMs\Virtual Machines\Local_DataNode4_64-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 20
+start vmrun start "C:\work\VMs\Virtual Machines\Local_Hadoop_64-bit\Local_Hadoop_64-bit.vmx"
+TIMEOUT /T 10
+start vmrun start "C:\work\VMs\Virtual Machines\Local_HUE\Local_HUE.vmx"
